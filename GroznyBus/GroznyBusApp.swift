03c466ffcb69b75961c46f311662d4bedb852328
Lukman <lukmanmakhaev@gmail.com>
2024-06-20 22:41:23 +0300

    Initial Commit

---
+++ GroznyBus/GroznyBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroznyBusApp.swift
+//  GroznyBus
+//
+//  Created by Lukman Makhaev on 20.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct GroznyBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
